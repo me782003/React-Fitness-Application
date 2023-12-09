c03c98b26ef17c68383995181f31b53265c70335
me782003 <me782003f@gmail.com>
2023-12-09 07:57:06 +0200

    add .gitignore file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+import React , { useEffect, useState } from "react";
+
+function App() {
+
+
+
+  return (
+    <div>
+      hello there~~
+
+
+    </div>
+  );
+}
+export default App;
+
